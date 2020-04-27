8d774953380972fafb3288018876373b30f387a6
thecassion <pierrerobentz.cassion@gmail.com>
2020-04-27 01:52:09 -0400

    Starting with read_departement_report function

--- helper.py
+++ helper.py
@@ -1,7 +1,15 @@
 from datetime import datetime , timedelta
+import tabula
+
 
 def increment_msppdate(str_date):
     datetime_object = datetime.strptime(str_date, '%d-%m-%Y')
     datetime_inc = datetime_object + timedelta(days=1)
     date_s = datetime_inc.strftime("%d-%m-%Y")
     return {"str":date_s,"datetime": datetime_inc}
+
+
+def read_department_report(url):
+    df = tabula.read_pdf(url, pages="all", multiple_tables=True)
+    print(type(df[1]))
+    return df[1]
--- test_helper.py
+++ test_helper.py
@@ -1,9 +1,14 @@
 import unittest
-from helper import increment_msppdate
+from helper import increment_msppdate , read_department_report
+import pandas as pd
 
 class TestHelper(unittest.TestCase):
     def test_increment_msppdatedef(self):
         self.assertEqual(increment_msppdate('29-02-2020')["str"],"01-03-2020")
+
+    def test_read_department_report(self):
+        url = "https://mspp.gouv.ht/site/downloads/Sitrep 05-04-2020.pdf"
+        self.assertTrue(isinstance(read_department_report(url), pd.DataFrame))
 
 if __name__ == '__main__':
     unittest.main()
