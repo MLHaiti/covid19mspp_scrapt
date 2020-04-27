1215ea07830e9d1c9d701619b0b1ed1436e1c7ad
thecassion <pierrerobentz.cassion@gmail.com>
2020-04-27 03:01:51 -0400

    Verify if a link is pdf or not

--- test_helper.py
+++ test_helper.py
@@ -1,5 +1,5 @@
 import unittest
-from helper import increment_msppdate , read_department_report
+from helper import increment_msppdate , read_department_report, is_pdf_link
 import pandas as pd
 
 class TestHelper(unittest.TestCase):
@@ -9,6 +9,11 @@ class TestHelper(unittest.TestCase):
     def test_read_department_report(self):
         url = "https://mspp.gouv.ht/site/downloads/Sitrep 05-04-2020.pdf"
         self.assertTrue(isinstance(read_department_report(url), pd.DataFrame))
+
+    def test_is_pdf_link(self):
+        url = "https://mspp.gouv.ht/site/downloads/Sitrep 05-04-2020.pdf"
+        self.assertTrue(is_pdf_link(url))
+        self.assertFalse(is_pdf_link('https://mspp.gouv.ht/site/downloads/Sitrep 05'))
 
 if __name__ == '__main__':
     unittest.main()
