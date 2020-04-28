0f535020af435ff5266aa7338c625c47f386d0cf
thecassion <pierrerobentz.cassion@gmail.com>
2020-04-28 13:43:06 -0400

    Append dataframe to get all report that tabular-py can read

--- application.py
+++ application.py
@@ -14,10 +14,14 @@ from helper import increment_msppdate, read_department_report, get_right_covid19
 
 #URL format for Bulletin du 7 Avril 2020 de la surveillance du nouveau Coronavirus 2019(COVID-19)
 
-start_date = '04-04-2020'
+start_date = '01-04-2020'
+
+
 
 
 data = get_right_covid19links(start_date)
+#init_date ='04-04-2020'
 
 
+#df = read_department_report('https://mspp.gouv.ht/site/downloads/Sitrep '+init_date+'.pdf')
 
